bebe43e82239f96135e442037ae0edeb4ce18826
Liutauras Razma <leq382121@gmail.com>
2020-06-23 15:10:24 +0300

    master working on recursion

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from "react";
 
 
 function App() {
-
-  // butu gerai prideti ID del react keys
+  // Hooks, nes jei pridesime Value, atitinkamai atsinaujins ir "state", tada rereshinsis puslapis.
+  // Case 1 (MVP)
   const [jsonValue, editJsonValue] = useState([
     {
       id: 0,
@@ -33,38 +33,108 @@ function App() {
     }
   ]);
 
+  // Case 2 (Recursion)
+  const [recursionValue, editrecursionValue] = useState([
+    {
+      id: 0,
+      level: 0,
+      name: 'Lorem',
+      childs: [
+        {
+          id: 1,
+          level: 1,
+          name: 'Ipsum'
+        },
+        {
+          id: 2,
+          level: 2,
+          name: 'Sit'
+        },
+      ]
+    },
+    {
+      id: 3,
+      level: 0,
+      name: 'Bis',
+      childs: [
+        {
+          id: 4,
+          level: 1,
+          name: 'Quit'
+        },
+        {
+          id: 5,
+          level: 2,
+          name: 'Wix'
+        },
+      ]
+    },
+    {
+      id: 3,
+      level: 0,
+      name: 'noChilds',
+      childs: []
+    }
+  ]);
+
 
   // tikrinam ar gautas key value yra objektas. 
   // jei tai nera objektas, grazinam. 
   // jei tai objektas, inicijuojame ta pacia funkcija su nauju objektu ir loopinam.
 
+  const Recursion = () => {
+    // Sukurkime array ir pagal 'level' upushinkime i array vidu pagal array indexa.
+    // f.ex: [[key:prop], [[key:prop], [key:prop]]]
 
-  // const rec = () => {
-  //   for (let value of Object.values(jsonValue)) {
-  //     console.log(value);
-  //     if (typeof value === 'object' && value !== null) {
-  //     }
-  //     return <li> - {value}</li>
-  //   }
-  // }
+    // recursionValue.map((item, index) => {
+    //   console.log('test');
+    //   if (!item.childs.length) {
+    //     console.log(item.childs.name);
 
-  // const arrayMap = () => {
-  //   jsonValue.map((item, index) => {
-  //     console.log(item.name);
-  //   })
-  // }
+    //     for (let i = 0; i < item.childs.length; i++) {
+    //       console.log(item.childs.name);
+
+    //       return item.childs[i].name;
+    //     }
+    //   }
+
+    //   return <li>{item.name}</li>
+    // })
+  }
 
   useEffect(() => {
   }, [jsonValue]);
 
   return ([
     <div className="objects-wrapper">
+      {/* ne pats gražiausias, bet MVP */}
+      <ul className="approach-1">
+        {jsonValue.map((item, index) => (
+          <li key={item.id} style={{ marginLeft: 10 * item.level }}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
 
-      {jsonValue.map((item, index) => (
-        <li key={index} style={{ marginLeft: 10 * item.level }}>
-          {item.name}
-        </li>
-      ))}
+      <ul className="approach-2">
+        {
+          recursionValue.map((item, index) => {
+            // console.log(!item.childs.length);
+
+            if (item.childs.length !== null) {
+              // console.log(item.childs.name);
+
+              for (let i = 0; i < item.childs.length; i++) {
+                console.log(item.childs[i].name);
+
+                return <li>{item.childs[i].name}</li>
+              }
+            }
+
+            return <li>{item.name}</li>
+          })
+        }
+      </ul>
 
     </div>
   ]);
